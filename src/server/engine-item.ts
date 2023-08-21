597ea713b8ab34cc30edad4fcf7592c41fe66fd1
NonSpicyBurrito <burritoyangster@gmail.com>
2023-08-21 18:46:40 +0800

    Updated to support Sonolus 0.7.3

--- src/server/engine-item.ts
+++ src/server/engine-item.ts
@@ -29,6 +29,7 @@ export const toEngineItem = (
     ),
     thumbnail: info.thumbnail,
     playData: info.playData,
+    previewData: info.previewData,
     tutorialData: info.tutorialData,
     rom: info.rom,
     configuration: info.configuration,
