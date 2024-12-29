56b45b4538f9f19ce7c6ced599d5f2fd9580a4a8
Burrito <burritoyangster@gmail.com>
2024-12-29 18:00:33 +0800

    Increased version number to 5.4.6

--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ import { parse } from './utils/json'
 
 const options = new Command()
     .name('sonolus-generate-static')
-    .version('5.4.5')
+    .version('5.4.6')
     .option('-i, --input <value>', 'input directory', 'pack')
     .option('-o, --output <value>', 'output directory', 'static')
     .option('-a, --address [value]', 'address')
